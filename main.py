359472f6b10c267ef7b47ca24ca6ab40eb584950
Daniel Huss <danhuss@users.noreply.github.com>
2025-10-07 23:34:31 -0400

    - Small Main.py cleanup - More package management

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 
 # class YatpState(TypedDict):
 #     messages: Annotated[list, add_messages]
+import sys
+import os
+from dotenv import load_dotenv
 
 from typing import Optional
 from langgraph.prebuilt import create_react_agent
@@ -22,49 +25,47 @@ def book_flight(from_airport: str, to_airport: str):
     """Book a flight"""
     return f"Successfully booked a flight from {from_airport} to {to_airport}."
 
-model = ChatOllama(model="llama3.2")
+def main():
+    load_dotenv()
+    model = ChatOllama(model="llama3.2")
 
-activity_agent = create_react_agent(
-    model=model,
-    tools=[activity_search],
-    prompt="Suggest an activity in a city based on user preferences.",
-    name="activity_agent"
-)
+    activity_agent = create_react_agent(
+        model=model,
+        tools=[activity_search],
+        prompt="Suggest an activity in a city based on user preferences.",
+        name="activity_agent"
+    )
 
-hotel_agent = create_react_agent(
-    model=model,
-    tools=[book_hotel],
-    prompt="You are a hotel booking assistant.",
-    name="hotel_agent"
-)
+    hotel_agent = create_react_agent(
+        model=model,
+        tools=[book_hotel],
+        prompt="You are a hotel booking assistant.",
+        name="hotel_agent"
+    )
 
-flight_agent = create_react_agent(
-    model=model,
-    tools=[book_flight],
-    prompt="You are a flight booking assistant.",
-    name="flight_agent"
-)
+    flight_agent = create_react_agent(
+        model=model,
+        tools=[book_flight],
+        prompt="You are a flight booking assistant.",
+        name="flight_agent"
+    )
 
-supervisor_agent = create_supervisor(
-    agents=[activity_agent, hotel_agent, flight_agent],
-    model=model,
-    prompt="You manage a small travel agency with an activity search agent, flight booking agent, and hotel booking agent. Assign work to them."
-).compile()
+    supervisor_agent = create_supervisor(
+        agents=[activity_agent, hotel_agent, flight_agent],
+        model=model,
+        prompt="You manage a small travel agency with an activity search agent, flight booking agent, and hotel booking agent. Assign work to them."
+    ).compile()
 
-png_data = supervisor_agent.get_graph().draw_mermaid_png()
-with open("graph.png", "wb") as f:
-    f.write(png_data)
+    png_data = supervisor_agent.get_graph().draw_mermaid_png()
+    with open("graph.png", "wb") as f:
+        f.write(png_data)
 
-# result = supervisor_agent.invoke(
-#     {"messages": [{"role": "user", "content": "I want to visit a Miami FL, what should I do? Then go ahead and book a flight from LA to Miami and a stay at Motel 6."}]}
-# )
+    result = supervisor_agent.invoke(
+        {"messages": [{"role": "user", "content": "I want to visit a Miami FL, what should I do? Then go ahead and book a flight from LA to Miami and a stay at Motel 6."}]}
+    )
 
-# print(result)
-# print(result.get('messages')[-1].content)
+    print(result)
+    # print(result.get('messages')[-1].content)
 
-# def main():
-#     print("Hello from lang-yatp!")
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
